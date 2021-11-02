ece4f23451635bacea533f9e41fdf464e2167b1e
Armanda <ceedadev@gmail.com>
2021-11-02 18:43:10 +0700

    calculate feature done

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final TextEditingController c100000 = TextEditingController();
+  final TextEditingController c50000 = TextEditingController();
+  final TextEditingController c75000 = TextEditingController();
+  final TextEditingController c20000 = TextEditingController();
+  final TextEditingController c10000 = TextEditingController();
+  final TextEditingController c5000 = TextEditingController();
+  final TextEditingController c2000 = TextEditingController();
+  final TextEditingController c1000 = TextEditingController();
+  final TextEditingController c500 = TextEditingController();
+  final TextEditingController c200 = TextEditingController();
+  final TextEditingController c100 = TextEditingController();
 
   final count = 0.obs;
+  final formatCount = ''.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -15,6 +29,74 @@ class HomeController extends GetxController {
   }
 
   @override
-  void onClose() {}
+  void onClose() {
+    c100000.dispose();
+    c75000.dispose();
+    c50000.dispose();
+    c20000.dispose();
+    c10000.dispose();
+    c5000.dispose();
+    c2000.dispose();
+    c1000.dispose();
+    c500.dispose();
+    c200.dispose();
+    c100.dispose();
+  }
+
   void increment() => count.value++;
+
+  void calculate() {
+    var total = 0;
+    if (c100000.text != '') {
+      final n100000 = num.parse(c100000.text);
+      total += (100000 * n100000.toInt());
+    }
+    if (c75000.text != '') {
+      final n75000 = num.parse(c75000.text);
+      total += (75000 * n75000.toInt());
+    }
+    if (c50000.text != '') {
+      final n50000 = num.parse(c50000.text);
+      total += (50000 * n50000.toInt());
+    }
+    if (c20000.text != '') {
+      final n20000 = num.parse(c20000.text);
+      total += (20000 * n20000.toInt());
+    }
+    if (c10000.text != '') {
+      final n10000 = num.parse(c10000.text);
+      total += (10000 * n10000.toInt());
+    }
+    if (c5000.text != '') {
+      final n5000 = num.parse(c5000.text);
+      total += (5000 * n5000.toInt());
+    }
+    if (c2000.text != '') {
+      final n2000 = num.parse(c2000.text);
+      total += (2000 * n2000.toInt());
+    }
+    if (c1000.text != '') {
+      final n1000 = num.parse(c1000.text);
+      total += (1000 * n1000.toInt());
+    }
+    if (c500.text != '') {
+      final n500 = num.parse(c500.text);
+      total += (500 * n500.toInt());
+    }
+    if (c200.text != '') {
+      final n200 = num.parse(c200.text);
+      total += (200 * n200.toInt());
+    }
+    if (c100.text != '') {
+      final n100 = num.parse(c100.text);
+      total += (100 * n100.toInt());
+    }
+
+    print(total);
+    this.count.value = total;
+
+    var formatter = NumberFormat('###,###,000');
+
+    this.formatCount.value = formatter.format(total);
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import 'package:get/get.dart';
 
@@ -9,15 +11,141 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('CashCalc'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Table(
+                // border: TableBorder.all(),
+                columnWidths: {
+                  0: FlexColumnWidth(2),
+                  1: FlexColumnWidth(0.5),
+                  2: FlexColumnWidth(2),
+                  3: FlexColumnWidth(0.5),
+                },
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                children: [
+                  cashTableRow(
+                      denomination: '100.000',
+                      color: Colors.red,
+                      controller: controller.c100000),
+                  cashTableRow(
+                      denomination: '75.000',
+                      color: Colors.pink,
+                      controller: controller.c75000),
+                  cashTableRow(
+                    denomination: '50.000',
+                    color: Colors.blue,
+                    controller: controller.c50000,
+                  ),
+                  cashTableRow(
+                    denomination: '20.000',
+                    color: Colors.green,
+                    controller: controller.c20000,
+                  ),
+                  cashTableRow(
+                    denomination: '10.000',
+                    color: Colors.purple,
+                    controller: controller.c10000,
+                  ),
+                  cashTableRow(
+                    denomination: '5.000',
+                    color: Colors.yellow,
+                    controller: controller.c5000,
+                  ),
+                  cashTableRow(
+                    denomination: '2.000',
+                    color: Colors.grey,
+                    controller: controller.c2000,
+                  ),
+                  cashTableRow(
+                    denomination: '1.000',
+                    color: Colors.lime,
+                    controller: controller.c1000,
+                  ),
+                  cashTableRow(
+                    denomination: '500',
+                    color: Colors.lime,
+                    controller: controller.c500,
+                  ),
+                  cashTableRow(
+                    denomination: '200',
+                    color: Colors.lime,
+                    controller: controller.c200,
+                  ),
+                  cashTableRow(
+                    denomination: '100',
+                    color: Colors.lime,
+                    controller: controller.c100,
+                  ),
+                ],
+              ),
+              Obx(() => Text(controller.formatCount.string)),
+              ElevatedButton(
+                onPressed: () {
+                  controller.calculate();
+                },
+                child: Text('Hitung'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  TableRow cashTableRow({
+    Key? key,
+    required String denomination,
+    required Color color,
+    TextEditingController? controller,
+  }) {
+    return TableRow(children: [
+      Container(
+        height: 42,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(blurRadius: 1, offset: Offset(0, 1), color: Colors.grey)
+          ],
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            denomination,
+            style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        child: Center(
+          child: Text('x'),
+        ),
+      ),
+      Container(
+        height: 42,
+        margin: EdgeInsets.symmetric(vertical: 8),
+        child: TextField(
+          controller: controller,
+          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          textAlignVertical: TextAlignVertical.top,
+          textAlign: TextAlign.center,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ),
+      // IconButton(onPressed: () {}, icon: Icon(Icons.clear_rounded))
+    ]);
   }
 }
